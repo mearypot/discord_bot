9112d3f14b239c7028a62321244ef879fd211ba3
novelou <a23.acs3@g.chuo-u.ac.jp>
2024-09-29 21:56:32 +0900

    MESSAGE_CONTENTを追加

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent
 
 fun main(args: Array<String>) {
     val botToken = System.getenv("DISCORD_TOKEN")
-    val jda = JDABuilder.createDefault(botToken)
-        .enableIntents(GatewayIntent.GUILD_MESSAGES)
+    val jda = JDABuilder.createLight(botToken, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
         .setActivity(Activity.playing("ゲーム再起動管理"))
         .addEventListeners(BotLister())
         .build()
