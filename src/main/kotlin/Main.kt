644cb12f9971f823502624d56c44faaf83a1263e
novelou <a23.acs3@g.chuo-u.ac.jp>
2024-09-29 22:10:57 +0900

    例外処理を丁寧に

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent
 
 fun main(args: Array<String>) {
     val botToken = System.getenv("DISCORD_TOKEN")
-    val jda = JDABuilder.createLight(botToken, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
+    val jda = JDABuilder.createLight(botToken, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
         .setActivity(Activity.playing("ゲーム再起動管理"))
         .addEventListeners(BotLister())
         .build()
