026c9d01a1a16912747a2a700e192a9aa9a52375
novelou <a23.acs3@g.chuo-u.ac.jp>
2024-09-29 22:38:05 +0900

    最終

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent
 fun main(args: Array<String>) {
     val botToken = System.getenv("DISCORD_TOKEN")
     val jda = JDABuilder.createLight(botToken, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
-        .setActivity(Activity.playing("ゲーム再起動管理"))
+        .setActivity(Activity.playing("!restart"))
         .addEventListeners(BotLister())
         .build()
 }
