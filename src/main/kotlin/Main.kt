f6a2b26bd69b4a65b0375f639413b8c8e5ab25ad
novelou <a23.acs3@g.chuo-u.ac.jp>
2024-09-29 21:42:31 +0900

    contentDefaultからcontentLightへ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.requests.GatewayIntent
 
 fun main(args: Array<String>) {
     val botToken = System.getenv("DISCORD_TOKEN")
-    val jda = JDABuilder.createDefault(botToken)
+    val jda = JDABuilder.createLight(botToken,
+        GatewayIntent.GUILD_MESSAGES)
         .setActivity(Activity.playing("ゲーム再起動管理"))
         .addEventListeners(BotLister())
         .build()
