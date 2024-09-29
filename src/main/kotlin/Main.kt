48ca2fb8ad9b5b50ef130931a304c8216b5414d4
novelou <a23.acs3@g.chuo-u.ac.jp>
2024-09-29 21:49:45 +0900

    gatewayIntentを追加

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ import net.dv8tion.jda.api.requests.GatewayIntent
 
 fun main(args: Array<String>) {
     val botToken = System.getenv("DISCORD_TOKEN")
-    val jda = JDABuilder.createLight(botToken,
-        GatewayIntent.GUILD_MESSAGES)
+    val jda = JDABuilder.createDefault(botToken)
+        .enableIntents(GatewayIntent.GUILD_MESSAGES)
         .setActivity(Activity.playing("ゲーム再起動管理"))
         .addEventListeners(BotLister())
         .build()
